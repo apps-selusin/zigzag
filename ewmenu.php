bf4cc388a2e97e279febe348c225ff780eb15613
apps-selusin <apps.selusin@gmail.com>
2018-01-25 21:19:23 +0700

    1

--- ewmenu.php
+++ ewmenu.php
@@ -24,9 +24,9 @@ $RootMenu->AddMenuItem(32, "mci_Karantina", $Language->MenuPhrase("32", "MenuTex
 $RootMenu->AddMenuItem(33, "mci_BPOM", $Language->MenuPhrase("33", "MenuText"), "cf99_underconstruction.php", 12, "", TRUE, FALSE, TRUE);
 $RootMenu->AddMenuItem(34, "mci_LS", $Language->MenuPhrase("34", "MenuText"), "cf99_underconstruction.php", 12, "", TRUE, FALSE, TRUE);
 $RootMenu->AddMenuItem(10010, "mri_r015fdepo", $Language->MenuPhrase("10010", "MenuText"), "r01_deposmry.php", -1, "{77B51533-F1E4-4C23-925C-E363F9E1C0BE}", TRUE, FALSE, FALSE);
-$RootMenu->AddMenuItem(14, "mci_Status", $Language->MenuPhrase("14", "MenuText"), "", -1, "", TRUE, FALSE, TRUE);
-$RootMenu->AddMenuItem(38, "mci_TPS", $Language->MenuPhrase("38", "MenuText"), "cf99_underconstruction.php", 14, "", TRUE, FALSE, TRUE);
-$RootMenu->AddMenuItem(39, "mci_T2e_Lamong", $Language->MenuPhrase("39", "MenuText"), "cf99_underconstruction.php", 14, "", TRUE, FALSE, TRUE);
+$RootMenu->AddMenuItem(14, "mci_Pelabuhan", $Language->MenuPhrase("14", "MenuText"), "", -1, "", TRUE, FALSE, TRUE);
+$RootMenu->AddMenuItem(38, "mci_TPS", $Language->MenuPhrase("38", "MenuText"), "www.tps.co.id:81/webaccess/", 14, "", TRUE, FALSE, TRUE);
+$RootMenu->AddMenuItem(39, "mci_Teluk_Lamong", $Language->MenuPhrase("39", "MenuText"), "cf99_underconstruction.php", 14, "", TRUE, FALSE, TRUE);
 $RootMenu->AddMenuItem(40, "mci_Schedule", $Language->MenuPhrase("40", "MenuText"), "cf99_underconstruction.php", 14, "", TRUE, FALSE, TRUE);
 $RootMenu->AddMenuItem(41, "mci_Bea_Container", $Language->MenuPhrase("41", "MenuText"), "cf99_underconstruction.php", 14, "", TRUE, FALSE, TRUE);
 $RootMenu->AddMenuItem(15, "mci_Trucking", $Language->MenuPhrase("15", "MenuText"), "", -1, "", TRUE, FALSE, TRUE);
